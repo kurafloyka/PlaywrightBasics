fece894b1e2eecc564875e4a5d0384649611d74d
Faruk <farukakyol441@gmail.com>
2023-12-23 18:41:08 +0300

    Added video config.

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    video: 'on',
     trace: 'on-first-retry',
   },
 
