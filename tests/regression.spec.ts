a5b0e296feb78ac946f8749ddc7fde81d4bb8c88
Faruk <farukakyol441@gmail.com>
2023-12-23 11:02:54 +0300

    Added highlight option to code.

--- tests/regression.spec.ts
+++ tests/regression.spec.ts
@@ -21,15 +21,30 @@ test.describe("Home Page", () => {
       .locator("#filterOrderSelect")
       .selectOption({ label: "Fiyata Göre (Artan)" });
 
-    //await expect(page.getByText("Fiyata Göre (Artan)")).toBeVisible();
+
+      // Manipulate the DOM and change the color of an element
+      await page.evaluate(() => {
+        // Select an element by its CSS selector and set styles
+        const element = document.getElementById('filterOrderSelect');
+        if (element) {
+          element.style.backgroundColor = 'lightblue';
+          element.style.color = 'yellow';
+          // Add more styles as needed
+        }
+      });
+
+
     //Sıralama alanının değiştiği gözlenir
 
+    //await page.pause();
     await page.getByText("Filtreleme").nth(2).click();
 
     await page.getByRole("link", { name: "Kargo Pantolon" }).click();
-    await expect(
-      page.getByRole("heading", { name: "Kargo Pantolon", exact: true })
-    ).toBeVisible();
+
+    //soft assertion
+    await expect
+      .soft(page.getByRole("heading", { name: "Kargo Pantolon", exact: true }))
+      .toBeVisible();
 
     await page
       .getByRole("link", { name: "CARGO Dokuma Comfort Siyah" })
@@ -53,7 +68,9 @@ test.describe("Home Page", () => {
       .getByText("Sepeti Temizle");
     await sepetiTemizleButton.click();
 
-    const yesButton = await page.frameLocator('.fancybox-iframe').locator("//button[@class='confirm']");
+    const yesButton = await page
+      .frameLocator(".fancybox-iframe")
+      .locator("//button[@class='confirm']");
     await yesButton.click();
 
     //page.getByRole("button", { name: "Evet",exact:true }).click();
@@ -62,11 +79,28 @@ test.describe("Home Page", () => {
       .getByText("Sepeti Temizle");
     await sepetiTemizleButton1.click();
 
-    const yesButton1 = await page.frameLocator('.fancybox-iframe').locator("//button[@class='confirm']");
+    const yesButton1 = await page
+      .frameLocator(".fancybox-iframe")
+      .locator("//button[@class='confirm']");
     await yesButton1.click();
 
     await expect(
-      await page.frameLocator('.fancybox-iframe').getByText("Sepetinizde ürün bulunmamaktadır.")
+      await page
+        .frameLocator(".fancybox-iframe")
+        .getByText("Sepetinizde ürün bulunmamaktadır.")
     ).toBeVisible();
+
+    await page
+      .frameLocator(".fancybox-iframe")
+      .getByText("Sepetinizde ürün bulunmamaktadır.")
+      .highlight();
+
+
+
+
+
   });
 });
+
+
+
